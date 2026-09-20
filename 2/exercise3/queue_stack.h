d89e79a0bcad3d70914a68164a0ce787b375ca5f
August D. Iversen <au792290@uni.au.dk>
2026-09-20 13:24:11 +0200

    Cleaning exercise 3

--- 2/exercise3/queue_stack.h
+++ 2/exercise3/queue_stack.h
@@ -1,24 +1,44 @@
-#include "queue_class.h"
+#ifndef _QUEUE_STACK_H_
+#define _QUEUE_STACK_H_
 
+#include <cassert>
+#include "stack_class.h"
+
+// Queue (FIFO) implemented with two Stacks (LIFO).
 template <typename Object>
-class QueueStack : public Queue<Object>
-{
-private:
-    Stack<Object> in;
-    Stack<Object> out;
+class QueueStack {
+  private:
+	Stack<Object> out;   // front of the queue is on top - we always read from here
+	Stack<Object> in;    // scratch space, empty between operations
 
-public:
-    void enqueue(Object x) {in.push(x);}
-    void dequeue() {
-        if (in.empty()) {
-            std::cout << "Queue empty!";
-        }
-        else {
-            out.push(in.top());
-            in.pop()
-        }}
-    T front() {
-        return in.top();
-    }
-    
-}
+  public:
+	bool empty() { return out.empty(); }
+
+	Object front() {
+		assert(!empty());
+		return out.top();
+	}
+
+	Object get() {
+		assert(!empty());
+		return out.pop();   // pop() returns the element
+	}
+
+	// The new element must end up at the bottom of 'out', to ensure FIFO.
+	// 1) Unload 'out' onto 'in'.
+	// 2) Put x into the now-empty 'out' - it becomes the bottom element.
+	// 3) Pour 'in' back on top of it.
+	void put(const Object x) {
+		while (!out.empty()) {
+			in.push(out.pop());
+		}
+
+		out.push(x);
+
+		while (!in.empty()) {
+			out.push(in.pop());
+		}
+	}
+};
+
+#endif
