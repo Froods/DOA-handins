e5e8e9b0db4f92819000231a8337f7a5c71d4799
August D. Iversen <au792290@uni.au.dk>
2026-09-21 12:59:22 +0200

    Formatting with alt + shft + f

--- 2/exercise3/main.cpp
+++ 2/exercise3/main.cpp
@@ -1,21 +1,21 @@
 #include "queue_stack.h"
 #include <iostream>
 
-//test program for queue with stacks
-int main(int argc, char *argv[]) {
-QueueStack<int> q;
+// test program for queue with stacks
+int main(int argc, char *argv[])
+{
+    QueueStack<int> q;
 
-std::cout << "Is queue empty? (1 = true, 0 = false) " << q.empty() << std::endl;
+    std::cout << "Is queue empty? (1 = true, 0 = false) " << q.empty() << std::endl;
 
+    q.put(5);
+    q.put(7);
+    q.put(6);
 
-q.put(5);
-q.put(7);
-q.put(6);
+    std::cout << "Is queue empty after put? " << q.empty() << std::endl;
 
-std::cout << "Is queue empty after put? " << q.empty() << std::endl;
+    std::cout << "Front of the queue, should be 5: " << q.front() << std::endl;
 
-std::cout << "Front of the queue, should be 5: " << q.front() << std::endl;
-
-std::cout << "Get() test, should pop 5: " << q.get() << "\nFront of queue should be 7 now: " << q.front();
-return 0;
+    std::cout << "Get() test, should pop 5: " << q.get() << "\nFront of queue should be 7 now: " << q.front();
+    return 0;
 }
--- 2/exercise3/queue_stack.h
+++ 2/exercise3/queue_stack.h
@@ -2,40 +2,46 @@
 #define _QUEUE_STACK_H_
 
 #include <cassert>
-#include "stack_class.h"
+#include "stack_class.h" // Stack class used from git
 
 // Queue (FIFO) implemented with two Stacks (LIFO).
 template <typename Object>
-class QueueStack {
-  private:
-	Stack<Object> out;   // front of the queue is on top - we always read from here
-	Stack<Object> in;    // scratch space, empty between operations
+class QueueStack
+{
+private:
+	Stack<Object> out; // front of the queue is on top - data is read from here
+	Stack<Object> in;  // in is only used in put() method
 
-  public:
+public:
 	bool empty() { return out.empty(); }
 
-	Object front() {
+	Object front()
+	{
 		assert(!empty());
 		return out.top();
 	}
 
-	Object get() {
+	Object get()
+	{
 		assert(!empty());
-		return out.pop();   // pop() returns the element
+		return out.pop(); // pop() returns the element
 	}
 
 	// The new element must end up at the bottom of 'out', to ensure FIFO.
 	// 1) Unload 'out' onto 'in'.
 	// 2) Put x into the now-empty 'out' - it becomes the bottom element.
 	// 3) Pour 'in' back on top of it.
-	void put(const Object x) {
-		while (!out.empty()) {
+	void put(const Object x)
+	{
+		while (!out.empty())
+		{
 			in.push(out.pop());
 		}
 
 		out.push(x);
 
-		while (!in.empty()) {
+		while (!in.empty())
+		{
 			out.push(in.pop());
 		}
 	}
